f0e029abb39e22be7fe6daed4949f05d6e31d180
Madhurima Gangireddy <116661081+Madhurima03124@users.noreply.github.com>
2025-01-16 21:43:53 +0530

    Update main.py

--- Assignment 3/translatorr/main.py
+++ Assignment 3/translatorr/main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,4 +7,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
